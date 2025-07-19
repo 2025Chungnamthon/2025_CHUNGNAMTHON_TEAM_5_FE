173ac58cb7a9cf18bb0a3b6634ac511a70a48f0d
Alal11 <spalqj56@naver.com>
2025-07-19 22:16:56 +0900

    feat: 모임 생성 페이지 라우팅 연결 #4
    - /create-meeting 경로 추가
    - 홈페이지 플로팅 버튼에서 모임 생성 페이지로 네이게이션 연결

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import {Routes, Route} from "react-router-dom";
 import Layout from "../layout/Layout";
 import Homepage from "../pages/Homepage/Homepage";
 import MeetingListPage from "../pages/MeetingList/MeetingListPage";
+import CreateMeetingPage from "../pages/CreateMeeting/CreateMeetingPage";
 // import Map from "../pages/Map";
 // import Chat from "../pages/Chat";
 // import MyPage from "../pages/MyPage";
@@ -10,9 +11,10 @@ import MeetingListPage from "../pages/MeetingList/MeetingListPage";
 
 const AppRouter = () => (
     <Routes>
-        <Route element={<Layout />}>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/meetings" element={<MeetingListPage />} />
+        <Route element={<Layout/>}>
+            <Route path="/" element={<Homepage/>}/>
+            <Route path="/meetings" element={<MeetingListPage/>}/>
+            <Route path="/create-meeting" element={<CreateMeetingPage/>}/>
             {/*
       <Route path="/map" element={<Map />} />
       <Route path="/chat" element={<Chat />} />
